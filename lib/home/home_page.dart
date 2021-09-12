83dcdea5ca11029882cde7e1f3445d437834573f
EsZhangHome <zhangensheng93@gmail.com>
2021-09-12 13:51:26 +0800

    hello word

---
+++ lib/home/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'hello flutter',
+              style: TextStyle(
+                color: Colors.blue, 
+                fontSize: 40.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
